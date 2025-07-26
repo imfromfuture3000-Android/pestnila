d187566e633113e8fe85c85be64a31d7a3086894
Nuno Maduro <enunomaduro@gmail.com>
2025-07-25 21:29:26 -0600

    chore: updates snapshots

--- src/Support/Description.php
+++ src/Support/Description.php
@@ -6,8 +6,14 @@ namespace Pest\Support;
 
 final readonly class Description implements \Stringable
 {
+    /**
+     * Creates a new Description instance.
+     */
     public function __construct(private string $description) {}
 
+    /**
+     * Returns the description as a string.
+     */
     public function __toString(): string
     {
         return $this->description;
