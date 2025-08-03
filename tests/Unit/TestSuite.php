273edb864cb7c90455501d8c0953b3cd144a7b99
Nuno Maduro <enunomaduro@gmail.com>
2025-08-03 10:32:14 -0600

    chore: adjusts tests

--- tests/Unit/TestSuite.php
+++ tests/Unit/TestSuite.php
@@ -27,7 +27,7 @@ it('does not allow static closures', function () {
     $testSuite->tests->set($method);
 })->throws(
     TestClosureMustNotBeStatic::class,
-    'Test closure must not be static. Please remove the `static` keyword from the `bar` method in `foo`.',
+    'Test closure must not be static. Please remove the [static] keyword from the [bar] method in [foo].',
 );
 
 it('alerts users about tests with arguments but no input', function () {
